0f29dee4a49a7fb5a37d3fde3a0fbf34ca3d79a0
addthoriq <alvarro.thoriq@gmail.com>
2022-11-20 22:38:24 +0700

    Manifest

--- app/src/main/java/com/thoriq/demoandroidnotification/IntegrasiKamera.kt
+++ app/src/main/java/com/thoriq/demoandroidnotification/IntegrasiKamera.kt
@@ -1,21 +1,18 @@
 package com.thoriq.demoandroidnotification
 
-import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
-import android.provider.SyncStateContract.Constants
 import android.widget.ImageButton
 import android.widget.ImageView
-import androidx.activity.result.contract.ActivityResultContracts
 
 @Suppress("DEPRECATION")
 class IntegrasiKamera : AppCompatActivity() {
 
     private val cameraRequest = 1888
-    lateinit var imageView: ImageView
+    private lateinit var imageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
